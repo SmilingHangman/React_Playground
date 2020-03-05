a13429bb56ae9f4f99b2f324c171139eaddb3e36
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-03-05 11:50:30 +0200

    input field added

--- src/App.js
+++ src/App.js
@@ -11,21 +11,33 @@ function App() {
     setToggle(!toggle)
   }
 
-  let [rotate, setRotate] = useState(0)
+  const [rotate, setRotate] = useState(0)
+
+  const [colorInput, setColorInput] = useState("")
   
   return (
     <div className={classes.container}>
-      <div className={classes.testbox} style={{backgroundColor: color }}></div>
-      <button className={classes.redbutton} onClick={() => setColor("rgb(224, 21, 21)")}>RED</button>
-      <button className={classes.graybutton} onClick={() => setColor("rgb(161, 154, 154)")}>GRAY</button>
+      <div className={classes.boxholder}>
+        <div className={classes.testbox} style={{backgroundColor: color }}></div>
+        <button className={classes.redbutton} onClick={() => setColor("rgb(224, 21, 21)")}></button>
+        <button className={classes.graybutton} onClick={() => setColor("rgb(161, 154, 154)")}></button>
+      </div>
 
+      <div className={classes.boxholder}>
+        <div className={classes.testbox} onClick={toggleIt} style={toggle ? {backgroundColor: "rgb(224, 21, 21)", width: "150px", height: "150px"} : {backgroundColor: "rgb(161, 154, 154)", width: "200px", height: "200px"}}></div>
+        <div className={classes.testbox} onClick={toggleIt} style={toggle ? {backgroundColor: "rgb(161, 154, 154)", width: "200px", height: "200px"} : {backgroundColor: "rgb(224, 21, 21)", width: "150px", height: "150px"}}></div>
+        <button onClick={toggleIt}>Toggle</button>
+      </div>
 
-      <div className={classes.testbox} onClick={toggleIt} style={toggle ? {backgroundColor: "rgb(224, 21, 21)", width: "150px", height: "150px"} : {backgroundColor: "rgb(161, 154, 154)", width: "200px", height: "200px"}}></div>
-      <div className={classes.testbox} onClick={toggleIt} style={toggle ? {backgroundColor: "rgb(161, 154, 154)", width: "200px", height: "200px"} : {backgroundColor: "rgb(224, 21, 21)", width: "150px", height: "150px"}}></div>
-      <button onClick={toggleIt}>Toggle</button>
-
-      <div className={classes.testbox}  onMouseDown={() => setRotate(rotate + 45)} style={{transform: `rotate(${rotate}deg)`}}></div>
-      <button onMouseDown={() => setRotate(rotate + 45)}>Rotate</button>
+      <div className={classes.boxholder}>
+        <div className={classes.testbox}  onMouseDown={() => setRotate(rotate + 45)} style={{transform: `rotate(${rotate}deg)`}}></div>
+        <button onMouseDown={() => setRotate(rotate + 45)}>Rotate</button>
+      </div>
+      
+      <div className={classes.boxholder}>
+        <div className={classes.testbox}  style={{backgroundColor: colorInput}}></div>
+        <input type="text" onInput={event => setColorInput(event.target.value)}></input>
+      </div>
     </div>
   );
 }
